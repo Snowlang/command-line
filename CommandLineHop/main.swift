67337f36a8367f5a33b0c1e1b0196eef160106a6
Iman Zarrabian <iman@omts.fr>
2018-09-13 16:31:58 +0200

    Initial Commit

---
+++ CommandLineHop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineHop
+//
+//  Created by Iman Zarrabian on 13/09/2018.
+//  Copyright © 2018 One More Thing Studio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
